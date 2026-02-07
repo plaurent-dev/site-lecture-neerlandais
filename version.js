9ca909bd1e41640be44fda217098da309a274748
plaurent-dev <60066975+plaurent-dev@users.noreply.github.com>
2026-02-07 18:47:17 +0100

    Versioning 1.10

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // Configuration de version centralisée
-const APP_VERSION = '1.0.9';
+const APP_VERSION = '1.0.10';
 
 // Remplacer automatiquement la version dans les liens CSS au chargement
 document.addEventListener('DOMContentLoaded', function() {
